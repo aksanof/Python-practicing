facaf07021e286712ab57961a2e5c1a36ad8ae47
Shamil <s.aksanof@gmail.com>
2022-10-16 08:12:18 +0500

    Second commit

--- ideal_weight.py
+++ ideal_weight.py
@@ -1,24 +1,21 @@
-def man():
-    wgh =  50 + (h - 150) * 0.72 + (age - 21)/4
+def imt():
+    wgh =  50 + (h - 150) * k + (age - 21)/4
     return(wgh)
 
-def woman():
-    wgh =  50 + (h - 150) * 0.36 + (age - 21)/4
-    return(wgh)
-
-
-h = int(input('Введите ваш рост(см): '))
-age = int(input('Введите ваш возраст: '))
+h = float(input('Введите ваш рост(см): '))
+age = float(input('Введите ваш возраст: '))
 
 while True:
     s = input('Введите ваш пол (m/w): ')
     if s == 'Завершить':
         break
     if s == 'm':
-        ves = man()
-        print('Ваш идеальный вес: ' + str(ves))
+        k = 0.72
+        print('Ваш идеальный вес: ' + str(imt()))
+        break
     elif s == 'w':
-        ves = woman()
-        print('Ваш идеальный вес: ' + str(ves))
+        k = 0.36
+        print('Ваш идеальный вес: ' + str(imt()))
+        break
     else:
         print('Неправильно введен пол')
